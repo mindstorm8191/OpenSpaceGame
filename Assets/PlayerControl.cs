b7fa0a476dc706183c604070e1aac95f311fb2eb
mindstorm8191 <mindstorm8191@yahoo.com>
2022-11-20 13:47:05 -0500

    Update jumping
    Updated code for jumping. It works sometimes, but sometimes the player falls through the plane

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     float moveSpeed = 0.1f;
-    float jumpForce = 30f;
     float turn;
     float sensitivity = 3f;
-    float gravity = 1f;
-    float jumpPower = 30f;
+    float gravity = 0.02f;
+    float jumpPower = 3f;
     float jumpSpeed;
 
     // Start is called before the first frame update
@@ -27,22 +26,38 @@ public class PlayerControl : MonoBehaviour
         transform.localRotation = Quaternion.Euler(0, turn, 0);
 
         float distance = GetComponent<Collider>().bounds.extents.y;  // this is the same for up & down
-        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, distance + 1f);
-        Debug.Log(isGrounded);
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, distance + 0.4f);
+        //Debug.Log(isGrounded);
 
         if (isGrounded)
         {
-            jumpSpeed = 0;
+            //jumpSpeed = 0;
             if (Input.GetButtonDown("Jump"))
             {
                 jumpSpeed = jumpPower;
+                transform.Translate(0f, jumpSpeed, 0f);
+                //Debug.Log("Jump!");
+            }else{
+                jumpSpeed = 0;
             }
         }
         else
         {
-            jumpSpeed -= gravity;
+            // Raycast to the ground, to determine how far it is to reach it. If it's close enough, we need to
+            // land on the ground, instead of going through it
+            Debug.Log(jumpSpeed);
+            RaycastHit hitInfo;
+            bool groundNear = Physics.Raycast(transform.position, Vector3.down, out hitInfo, distance + 1f + jumpSpeed +gravity);
+            if(groundNear && jumpSpeed<0) {
+                //hitInfo.point.y tells us where we will stop falling at
+                transform.Translate(0f, (transform.position.y-distance) -hitInfo.point.y, 0f);
+                //if(jumpSpeed<0)
+                jumpSpeed = 0;
+            }else{
+                transform.Translate(0f, jumpSpeed, 0f);
+                jumpSpeed -= gravity;
+            }
         }
-
-        transform.Translate(Input.GetAxis("Horizontal") * moveSpeed, jumpSpeed, Input.GetAxis("Vertical") * moveSpeed);
+        transform.Translate(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
     }
 }
